c98546fefd6377931dbaad786bd55615a7f9a138
Morton Jonuschat <mjonuschat@gmail.com>
2024-05-03 18:52:46 -0700

    fix: Mark describe method as unused for now

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -2,6 +2,7 @@ pub(crate) mod klipper;
 pub(crate) mod moonraker;
 
 pub(crate) trait MetricsExporter {
+    #[allow(dead_code)]
     fn describe(&self) {}
     fn export(&self, _name: Option<&String>) {}
 }
